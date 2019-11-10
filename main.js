8c2489a587e9ee5338825a47c77d4bb25ecd9125
akekaluck.wesnarat <akekaluck.wesnarat@thomsonreuters.com>
2019-11-10 13:07:57 +0700

    drag drop example

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-const { app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, Menu, ipcMain} = require('electron');
+const dockMenu = Menu.buildFromTemplate([
+  {
+    label: 'New Window',
+    click () { console.log('New Window') }
+  }, {
+    label: 'New Window with Settings',
+    submenu: [
+      { label: 'Basic' },
+      { label: 'Pro' }
+    ]
+  },
+  { label: 'New Command...' }
+])
+
+// app.dock.setMenu(dockMenu);
+
+console.log('hi')
+
+ipcMain.on('ondragstart', (event, filePath) => {
+  console.log(`ondragstart ${filePath}`);
+  event.sender.startDrag({
+    file: filePath,
+    icon: 'angularjs.png'
+  })
+})
 
 app.on('ready', () => {
+    app.dock.setMenu(dockMenu);
     let win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -9,8 +35,13 @@ app.on('ready', () => {
           nodeIntegration: true
         }
       })
-    
+      win.webContents.on('did-finish-load', () => {
+        win.webContents.send('ping', 'whoooooooh!')
+      })
       // and load the index.html of the app.
       win.loadFile('index.html')
+      win.openDevTools()
       // win.loadURL('https://wwww.pantip.com')
+      // and load the index.html of the app.
+      // win.setProgressBar(0.5)
 })
